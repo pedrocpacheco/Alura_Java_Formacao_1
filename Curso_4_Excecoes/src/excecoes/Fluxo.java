68f4b6122dc3a32669e894bd50991680ddef4c18
Pedro C. Pacheco <cpedropacheco@hotmail.com>
2023-03-19 12:57:34 -0300

    comecando curso 4

---
+++ Curso_4_Excecoes/src/excecoes/Fluxo.java
@@ -0,0 +1,25 @@
+package excecoes;
+
+public class Fluxo {
+
+	public static void main(String[] args) {
+		System.out.println("Inicio do Main");
+		metodo1();
+		System.out.println("Fim do Main");
+	}
+	
+	public static void metodo1() {
+		System.out.println("Inicio metodo 1");
+		metodo2();
+		System.out.println("Fim do metodo 1");
+	}
+	
+	public static void metodo2() {
+		System.out.println("Inicio metodo 2");
+		for(int i = 1; i <= 5; i++) {
+			System.out.println(i);
+		}
+		System.out.println("Fim do metodo 2");
+	}
+	
+}
