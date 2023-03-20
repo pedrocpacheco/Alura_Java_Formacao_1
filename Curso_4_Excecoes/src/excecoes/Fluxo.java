e36b927dd1d9f1d4835cb3b8f7f3fac44c6f4ec3
Pedro C. Pacheco <cpedropacheco@hotmail.com>
2023-03-20 14:35:15 -0300

    usando try e catch para tratar excecao

--- Curso_4_Excecoes/src/excecoes/Fluxo.java
+++ Curso_4_Excecoes/src/excecoes/Fluxo.java
@@ -7,20 +7,24 @@ public class Fluxo {
 		metodo1();
 		System.out.println("Fim do Main");
 	}
-	
+
 	public static void metodo1() {
 		System.out.println("Inicio metodo 1");
+		try {
 		metodo2();
+		}catch(ArithmeticException e) {
+			System.out.println("ArithmeticException");
+		}
 		System.out.println("Fim do metodo 1");
 	}
-	
+
 	public static void metodo2() {
 		System.out.println("Inicio metodo 2");
-		for(int i = 1; i <= 5; i++) {
+		for (int i = 1; i <= 5; i++) {
 			System.out.println(i);
-			int a = i / 0;
+				int a = i / 0;
 		}
 		System.out.println("Fim do metodo 2");
 	}
-	
+
 }
