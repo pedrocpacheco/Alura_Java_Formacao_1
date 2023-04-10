ecc450eefa0db6713b89f5872bf313787809a868
Pedro C. Pacheco <cpedropacheco@hotmail.com>
2023-04-10 19:06:37 -0300

    starting to use arraylists

---
+++ Curso_6_Colecoes_Wrappes_Lambda/src/arrayList/TestArrayLists.java
@@ -0,0 +1,14 @@
+package arrayList;
+
+import java.util.ArrayList;
+
+import arrays.classes.Conta;
+
+public class TestArrayLists {
+
+	ArrayList list = new ArrayList();
+	
+	Conta account = new Conta(1, 32);
+	Conta account2 = new Conta(2, 32);
+	
+}
